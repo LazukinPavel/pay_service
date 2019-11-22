d34e0ce13f3d1f129a58f99f57611fd5c3d50275
backend <backend@analitika.online>
2019-11-22 14:59:25 +0300

    Preparing sign added. Protocol PAY added. Routes updated.

--- app/payment_processor.py
+++ app/payment_processor.py
@@ -1,16 +1,27 @@
+import hashlib
+import random
+
+
+currency_codes = {
+    'eur': 978,
+    'usd': 840,
+    'rur': 643
+}
+
 
 class PaymentProcessor:
-    def __init__(self, amount, currency, description):
+    def __init__(self, amount, currency, description, shop_id):
         self.amount = amount
-        self.currency = currency
+        self.currency = currency_codes[currency]
         self.description = description
+        self.shop_id = shop_id
 
     def processing(self):
-        if self.amount == 'EUR':
+        if self.currency == 978:
             self._process_eur()
-        if self.amount == 'USD':
+        if self.currency == 840:
             self._process_usd()
-        if self.amount == 'RUR':
+        if self.currency == 643:
             self._process_rur()
 
     def _process_rur(self):
@@ -20,4 +31,31 @@ class PaymentProcessor:
         pass
 
     def _process_eur(self):
-        pass
+        keys_required = ['amount', 'currency', 'shop_id', 'shop_order_id']
+        params = {
+            'amount': self.amount,
+            'currency': self.currency,
+            'shop_id': self.shop_id,
+            'shop_order_id': random.randrange(10000)
+        }
+
+
+
+
+    def get_sign(self, params, keys_required):
+        # params = {
+        #     "currency": "643",
+        #     "payway": "payeer_rub",
+        #     "amount": "12.34",
+        #     "shop_id": "5",
+        #     "shop_order_id": 4126,
+        #     "description": "Test invoice",
+        # }
+        # keys_required = ("shop_id", "payway", "amount", "currency", "shop_order_id")
+
+        keys_sorted = sorted(keys_required)
+        values = [str(params[k]) for k in keys_sorted]
+        pre_hash = ':'.join(values)
+        sign = hashlib.sha256(pre_hash.encode('utf-8')).hexdigest()
+        return sign
+
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, request, redirect, url_for
 from .payment_processor import PaymentProcessor
 
 from app import app
-from app.forms import PayForm
+from app.forms import PayForm, PayFormProtocolPAY
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -13,14 +13,19 @@ def pay_form():
         currency = form.currency.data
         description = form.description.data
 
-        p = PaymentProcessor(amount, currency, description)
+        p = PaymentProcessor(amount, currency, description, app.config['SHOP_ID'])
         p.processing()
 
         return redirect(url_for('submit'))
     return render_template('pay_form.html', title='Pay Form', form=form)
 
 
-@app.route('/submit')
+@app.route('/submit', methods=['GET', 'POST'])
 def submit():
-    # TODO handle form processing
-    return 'Accepted'
+    form = PayFormProtocolPAY()
+    if request.method == 'POST' and form.validate_on_submit():
+
+        # TODO pass form-data to piastrix
+        return redirect('https://pay.piastrix.com/en/pay')
+
+    return render_template('pay_form.html', title='Pay Form', form=form)
