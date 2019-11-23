6d306fa25c04f1712710d8fde2f066d5c3440c05
backend <backend@analitika.online>
2019-11-23 22:20:08 +0300

    Piastrix request method added.

--- app/piastrix.py
+++ app/piastrix.py
@@ -1,5 +1,6 @@
 import hashlib
 import random
+import requests
 
 from flask import current_app
 
@@ -10,53 +11,62 @@ class Piastrix:
         self.currency = currency
         self.description = description
 
-    def processing(self):
+    def request_piastrix(self, url, fields):
+        try:
+            resp = requests.post(url, json=fields).json()
+            print('=== resp', resp)
+        except ConnectionError as e:
+            return 'Sorry, we have connection problem'
+            # TODO log exception
+        return resp
+
+    def prepare_form_fields(self):
         if self.currency == '978':
-            params, keys_required = self._process_eur()
+            fields, keys_required = self._prepare_data_eur()
         elif self.currency == '840':
-            params, keys_required = self._process_eur()
+            fields, keys_required = self._prepare_data_usd()
         else:
-            params, keys_required = self._process_eur()
+            fields, keys_required = self._prepare_data_rur()
 
-        sign = self._get_sign(params, keys_required)
-        params['sign'] = sign
-        return params
+        sign = self._get_sign(fields, keys_required)
+        fields['sign'] = sign
+        return fields
 
-    def _process_rur(self):
+    def _prepare_data_rur(self):
         keys_required = ['amount', 'currency', 'payway', 'shop_id', 'shop_order_id']
-        params = {
+        fields = {
             'amount': self.amount,
             'currency': self.currency,
             'payway': 'payeer_rub',
             'shop_id': current_app.config['SHOP_ID'],
             'shop_order_id': random.randrange(10000)
         }
-        return params, keys_required
+        return fields, keys_required
 
-    def _process_usd(self):
+    def _prepare_data_usd(self):
         keys_required = ['shop_amount', 'shop_currency', 'shop_id', 'shop_order_id', 'payer_currency']
-        params = {
+        fields = {
             'shop_amount': self.amount,
             'shop_currency': self.currency,
             'shop_id': current_app.config['SHOP_ID'],
             'shop_order_id': random.randrange(10000),
             'payer_currency': self.currency
         }
-        return params, keys_required
+        return fields, keys_required
 
-    def _process_eur(self):
+    def _prepare_data_eur(self):
         keys_required = ['amount', 'currency', 'shop_id', 'shop_order_id']
-        params = {
+        fields = {
             'amount': self.amount,
             'currency': self.currency,
             'shop_id': current_app.config['SHOP_ID'],
             'shop_order_id': random.randrange(10000)
         }
-        return params, keys_required
+        return fields, keys_required
 
-    def _get_sign(self, params, keys_required):
+    def _get_sign(self, fields, keys_required):
         keys_sorted = sorted(keys_required)
-        values = [str(params[k]) for k in keys_sorted]
+        values = [str(fields[k]) for k in keys_sorted]
         pre_hash = ':'.join(values)
         pre_hash += current_app.config['SECRET_KEY']
         sign = hashlib.sha256(pre_hash.encode()).hexdigest()
